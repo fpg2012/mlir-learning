24244feb8c690671415c5754b228df53c50c8ecd
nth233 <fpg2012@foxmail.com>
2026-06-14 19:11:31 +0800

    add poly dialect

---
+++ lib/Dialect/Poly/PolyDialect.cpp
@@ -0,0 +1,12 @@
+#include "lib/Dialect/Poly/PolyDialect.h"
+#include "lib/Dialect/Poly/PolyDialect.cpp.inc"
+
+namespace mlir {
+namespace my_opt {
+namespace poly {
+
+void PolyDialect::initialize() {}
+
+}
+}
+}
---
+++ lib/Dialect/Poly/PolyDialect.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#include "mlir/include/mlir/IR/DialectImplementation.h"
+#include "lib/Dialect/Poly/PolyDialect.h.inc"
