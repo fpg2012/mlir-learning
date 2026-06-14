4e6f588a5e767ad424dfcd500ca82a38e9f4fb06
nth233 <fpg2012@foxmail.com>
2026-06-14 20:57:40 +0800

    add poly dialect to `my-opt`

--- tools/my-opt.cpp
+++ tools/my-opt.cpp
@@ -1,12 +1,14 @@
 #include "lib/Transform/Affine/AffineFullUnroll.h"
 #include "lib/Transform/Affine/AffineFullUnroll2.h"
 #include "lib/Transform/Arith/MulToAdd.h"
+#include "lib/Dialect/Poly/PolyDialect.h"
 #include "mlir/include/mlir/InitAllDialects.h"
 #include "mlir/include/mlir/Pass/PassRegistry.h"
 #include "mlir/include/mlir/Tools/mlir-opt/MlirOptMain.h"
 
 int main(int argc, char **argv) {
     mlir::DialectRegistry registry;
+    registry.insert<mlir::my_opt::poly::PolyDialect>();
     mlir::registerAllDialects(registry);
 
     mlir::PassRegistration<mlir::my_opt::AffineFullUnrollPass>();
